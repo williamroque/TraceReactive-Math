3c20d9301b9805a71ad2bb3fc413e86d9b89ac06
William Roque <william.aroque@gmail.com>
2026-09-21 23:21:21 -0300

    Replaced liteAdaptor with browserAdaptor for MathJax rendering.

--- src/nodes/latex/LatexRenderNode.ts
+++ src/nodes/latex/LatexRenderNode.ts
@@ -5,11 +5,11 @@ import { MathCategory } from '../../category';
 import { mathjax } from 'mathjax-full/js/mathjax.js';
 import { TeX } from 'mathjax-full/js/input/tex.js';
 import { SVG } from 'mathjax-full/js/output/svg.js';
-import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor.js';
+import { browserAdaptor } from 'mathjax-full/js/adaptors/browserAdaptor.js';
 import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html.js';
 import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages.js';
 
-const adaptor = liteAdaptor();
+const adaptor = browserAdaptor();
 RegisterHTMLHandler(adaptor);
 
 const texInput = new TeX({ packages: AllPackages });
